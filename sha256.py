e5d28bdc08cab544a04d0790446711cc925db9a6
Alkimyo <164453140+Alkimyo@users.noreply.github.com>
2025-01-10 22:42:18 +0500

    Add files via upload

---
+++ sha256.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import hashlib
+import streamlit as st
+
+# CSV faylini o'qish
+file_path = '/home/shohruh/Downloads/world_population.csv'
+df = pd.read_csv(file_path)
+
+# SHA-256 hisoblash funksiyasi
+def calculate_hash(original_data, check_data):
+    if original_data:
+        original_hash = hashlib.sha256(original_data.encode()).hexdigest()
+        st.write(f"Asl ma'lumotning SHA-256 xeshi: {original_hash}")
+    
+    if check_data:
+        check_hash = hashlib.sha256(check_data.encode()).hexdigest()
+        st.write(f"Tekshirish ma'lumotining SHA-256 xeshi: {check_hash}")
+        
+        if original_hash == check_hash:
+            st.success("Ma'lumot o'zgarmagan. Xeshlar mos!")
+        else:
+            st.error("Ma'lumot buzilgan! Xeshlar mos emas!")
+
+# CSV faylidan ma'lumot qidirish
+def search_data():
+    user_input = st.text_input("Iltimos, mamlakat nomini kiriting:")
+    if user_input:
+        matching_rows = df[df['Country/Territory'].str.contains(user_input, case=False, na=False)]
+        if not matching_rows.empty:
+            df_selected = matching_rows[['Country/Territory', 'Capital', '2022 Population', 'Area (km²)']]
+            df_selected['2022 Population'] = df_selected['2022 Population'].apply(lambda x: f"Odamlar soni: {x} ta")
+            df_selected['Area (km²)'] = df_selected['Area (km²)'].apply(lambda x: f"Maydoni: {x} km²")
+
+            result_message = ""
+            for _, row in df_selected.iterrows():
+                result_message += f"Mamlakat: {row['Country/Territory']}, Poytaxti: {row['Capital']}, {row['2022 Population']}, {row['Area (km²)']}\n"
+
+            st.text_area("Natija", result_message)
+        else:
+            st.warning("Kiritilgan nom bo'yicha mos ma'lumotlar topilmadi!")
+
+# Streamlit interfeysi
+st.title("SHA-256 xeshlash va ma'lumot qidirish")
+
+# Ma'lumotlarni kiritish
+original_data = st.text_input("Siz yuborayotgan ma'lumot:")
+check_data = st.text_input("Do'stingiz qabul qilayotgan ma'lumot:")
+
+if st.button("Xeshlarni taqqoslash"):
+    calculate_hash(original_data, check_data)
+
+if st.button("Mamlakatni qidirish"):
+    search_data()
